cfcc610f0d47d6ffd0b2cec8bdb7db8bf69d6380
bgfaraday <hannah@whiterabbit.com>
2024-03-20 20:06:34 +1300

    adding opendmarc

--- email-docker/postfix-init.sh
+++ email-docker/postfix-init.sh
@@ -1,22 +1,31 @@
+#https://github.com/cisagov/postfix-docker/blob/develop/src/docker-entrypoint.sh
 #!/bin/bash
 
-#generate confifunction generate_configs() {
+#generate confifunction
+generate_configs() {
   #put in the env variables
-  echo "Generating postfix configurations for ${SERVER_HOSTNAME}"
+  echo "Generate postfix configurations for ${SERVER_HOSTNAME}"
   envsubst '\$SERVER_HOSTNAME \$SERVER_IP' < src/postfix/main.cf > /etc/postfix/main.cf
   envsubst '\$SERVER_HOSTNAME \$SERVER_IP' < src/opendkim/opendkim.conf > /etc/opendkim.conf
 
   # generate opendkim
-  echo "Generating opendkim configurations for ${SERVER_HOSTNAME}"
+  echo "Generate opendkim configurations for ${SERVER_HOSTNAME}"
   mkdir -p /etc/opendkim/keys
   chown -R opendkim:opendkim /etc/opendkim
   mkdir -p "/etc/opendkim/keys/${SERVER_HOSTNAME}"
   opendkim-genkey -b 2048 -d ${SERVER_HOSTNAME} -D /etc/opendkim/keys/${SERVER_HOSTNAME} -s default -v
   envsubst '\$SERVER_HOSTNAME \$SERVER_IP' < src/opendkim/trusted.hosts > /etc/opendkim/trusted.hosts 
-  echo 'SOCKET="inet:12301"' >> /etc/default/opendkim
+  # Change Permission for the keys
   chown opendkim:opendkim /etc/opendkim/keys/${SERVER_HOSTNAME}/default.private
+  edit_signing_table
 
- 
+ # generate opendmarc
+  echo "Generate opendmarc configurations for ${SERVER_HOSTNAME}"
+  envsubst '\$SERVER_HOSTNAME \$SERVER_IP' < src/opendmarc/opendmarc.conf > /etc/opendkim.conf 
+  mkdir -p "/etc/opendmarc"
+  echo "localhost" > /etc/opendmarc/ignore.hosts
+  chown -R opendmarc:opendmarc /etc/opendmarc
+
   # configure dovecot
   echo "Generating dovecot configurations for ${SERVER_HOSTNAME}"
   envsubst '\$SERVER_HOSTNAME \$SERVER_IP' < src/dovecot/dovecot.conf > /etc/dovecot/dovecot.conf
@@ -29,18 +38,6 @@ function edit_signing_table (){
     #Edit signing.table
     echo "*@{$SERVER_HOSTNAME} default._domainkey.{$SERVER_HOSTNAME}" | sudo tee /etc/opendkim/signing.table > /dev/null
     echo "*@*.{$SERVER_HOSTNAME} default._domainkey.{$SERVER_HOSTNAME}" | sudo tee /etc/opendkim/signing.table > /dev/null
-
-}
-#Generate keys
-function generate_dkim_keys () {
-    mkdir -p /etc/opendkim/keys
-    chown -R opendkim:opendkim /etc/opendkim
-    chmod  744 /etc/opendkim/keys
-    mkdir /etc/opendkim/keys/{$SERVER_HOSTNAME}
-    #Create Selector Key
-    opendkim-genkey -b 2048 -d {$SERVER_HOSTNAME} -D /etc/opendkim/keys/{$SERVER_HOSTNAME} -s default -v
-    # Change Permission for the keys
-    chown opendkim:opendkim /etc/opendkim/keys/{$SERVER_HOSTNAME}/default.private
 
 }
 
@@ -57,116 +54,6 @@ function set_cloud_false () {
 sudo bash -c 'awk "/^preserve_hostname: false\$/ {\$2=\"true\"} 1" /etc/cloud/cloud.cfg > /etc/cloud/cloud.cfg.tmp && mv /etc/cloud/cloud.cfg.tmp /etc/cloud/cloud.cfg'
 }
 
-
-
-#Edit Postfix.conf
-function config_postfix(){
-    postconf -e 'inet_interfaces = all'
-    #set default action when message received
-    postconf -e 'milter_default_action = accept'
-    #Set protocol to be used when communicating with opendkim
-    postconf -e 'milter_protocol = 6'
-    #SMTPD_milters sets the list of milters postfix will use
-    postconf -e 'smtpd_milters = local:opendkim/opendkim.sock'
-    #specifies mail transmission for non smtp connections
-
-    postconf -e 'non_smtpd_milters = local:opendkim/opendkim.sock'
-    postconf -e 'smtpd_sasl_type = dovecot'
-    postconf -e 'smtpd_sasl_auth_enable = yes'
-    postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks,reject_unauth_destination'
-    postconf -e 'non_smtpd_milters = local:opendkim/opendkim.sock'
-}
-
-# Edit config file for dkim
-function config_opendkim(){
-    # Uncomment Mode
-    sed -i '/^#.*\bMode\b/s/^#//' /etc/opendkim.conf
-    # Uncomment Subdomains
-    sed -i '/^#.*\bSubDomains\b/s/^#//' /etc/opendkim.conf
-    # Uncomment KeyFile
-    sed -i '/^#.*\bKeyFile\b/s/^#//' /etc/opendkim.conf
-    # Uncomment Logwhy
-    sed -i '/^#.*\LogWhy\b/s/^#//' /etc/opendkim.conf
-    # Uncomment Subdomain
-    sed -i '/^#.*\bSubDomain\b/s/^#//' /etc/opendkim.conf
-    # Uncomment ExternaIgnoreList
-    sed -i '/^#.*\bExternalIgnoreList\b/s/^#//' /etc/opendkim.conf
-    # Uncomment InternalHosts
-    sed -i '/^#.*\bInternalHosts\b/s/^#//' /etc/opendkim.conf
-    
-    # restart if opendkim stops
-    echo "AutoRestart            yes" |  tee -a /etc/opendkim.conf
-    echo "AutoRestartRate        10/1M" |  tee -a /etc/opendkim.conf
-    
-    # run in background
-    echo "Background             yes" |  tee -a /etc/opendkim.conf
-    
-    echo "DNSTimeout             5" |  tee -a /etc/opendkim.conf
-    echo "SignatureAlgorithm     rsa-sha256" |  tee -a /etc/opendkim.conf
-    
-    # specify key tables and signing table locations
-    echo "KeyTable            refile:/etc/opendkim/key.table" |  tee -a /etc/opendkim.conf
-    echo "SigningTable     refile:/etc/opendkim/signing.table" |  tee -a /etc/opendkim.conf
-
-    # specify the file containing a list of hosts
-    echo "ExternalIgnoreList  /etc/opendkim/trusted.hosts" |  tee -a /etc/opendkim.conf
-    echo "InternalHosts       /etc/opendkim/trusted.hosts" |  tee -a /etc/opendkim.conf
-    #Edit key.table
-    echo "default._domainkey.{$SERVER_HOSTNAME}    {$SERVER_HOSTNAME}:default:/etc/opendkim/keys/{$SERVER_HOSTNAME}/default.private" | sudo tee /etc/opendkim/key.table > /dev/null
-
-    #Edit Trusted.hosts
-    echo -e "127.0.0.1\nlocalhost\nmail\nmail.{$SERVER_HOSTNAME}\n{$SERVER_HOSTNAME}"  | sudo tee /etc/opendkim/trusted.hosts  > /dev/null
-
-    # Configure the OpenDKIm socket location
-    mkdir /var/spool/postfix/opendkim 
-    chown opendkim:postfix /var/spool/postfix/opendkim 
-
-    sed -i 's/^#\(Socket\s\+local:\/var\/spool\/postfix\/opendkim\/opendkim.sock\)/\1/' /etc/opendkim.conf
-    sed -i 's|^SOCKET=local:$RUNDIR/opendkim.sock|SOCKET=local:/var/spool/postfix/opendkim/opendkim.sock|' /etc/default/opendkim
-}
-
-
-
-function config_dovecot (){
-    # configure dovecot to use sasl
-    sed -i '/^service auth {/a \
-        unix_listener /var/spool/postfix/private/auth { mode = 0666 }' /etc/dovecot/conf.d/10-master.conf
-        
-    # configure login authentication 
-    sed -i '/^auth_mechanisms = plain/ s/$/ login/' /etc/dovecot/conf.d/10-auth.conf
-}
-
-function restart_services()
-{
-    systemctl restart opendkim postfix
-    service dovecot restart
-    #Restart Postfix and DKIM
-    systemctl start opendkim
-}
-
-function test_config () {
-    # test if localhost on port 25 works
-   if echo "ehlo localhost" | telnet localhost 25 |   grep -q "250-AUTH PLAIN LOGIN" ; then
-        echo "Test passed: Set up succesful"
-    else
-        echo "Test failed: Set up failed"
-    fi
-
-    # test if postfix service is active
-    if systemctl is-active --quiet postfix; then
-        echo "Postfix is running."
-    else
-        echo "Postfix is not running."
-    fi
-
-    # test if opendkim service is active
-    if systemctl is-active --quiet opendkim; then
-        echo "opendkim is running."
-    else
-        echo "opendkim is not running."
-    fi
-}
-
 function output_keys (){
     echo "-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-"
     echo "Place the following output as the DKIM key in your DNS server, note that formatting may differ depending on DNS provider"
@@ -178,7 +65,7 @@ function output_keys (){
     echo
 
     echo "Place the following output as the DMARC key in your DNS server"
-    echo "v=DMARC1; p=reject; rua=mailto:user@$SERVER_HOSTNAME"
+    echo "v=DMARC1; p=reject; rua=mailto:mailarchive@$SERVER_HOSTNAME"
     echo "Add this as a new TXT record where the Record name is _dmarc.$SERVER_HOSTNAME"
     echo
 
@@ -212,6 +99,10 @@ function generate_users() {
 }
 
 if [ "$1" = 'postfix' ]; then
+
+  check_root
+  set_cloud_false
+
   echo "Starting mail server with:"
   echo "  SERVER_HOSTNAME=${SERVER_HOSTNAME}"
   echo "  RELAY_IP=${SERVER_IP}"
